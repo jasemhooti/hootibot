bb7a0e916651784a5e556b458efd04db6f1a0bfc
جاسم حوتی <59818587+jasemhooti@users.noreply.github.com>
2025-03-20 19:18:14 +0330

    Create config.example.php

---
+++ config.example.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'bot_token' => '6414210268:AAEL-RZiABoMzS_QY922hOQnpXcam9OgiF0', // توکن ربات خود را اینجا قرار دهید
+    'admin_id' => '5691972852', // آیدی عددی ادمین
+    'database' => [
+        'host' => 'localhost',
+        'dbname' => 'hootibot',
+        'username' => 'root',
+        'password' => ''
+    ],
+    'channel_lock' => [
+        'enabled' => true,
+        'channel_id' => '@yourchannel'
+    ],
+    'game' => [
+        'min_bet' => 500,
+        'max_bet' => 5000000
+    ]
+];
