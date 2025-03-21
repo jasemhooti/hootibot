c5cabbc30137d11229b6d7329cbdf23c60c6933b
جاسم حوتی <59818587+jasemhooti@users.noreply.github.com>
2025-03-21 09:56:41 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'bot_token' => '6414210268:AAEL-RZiABoMzS_QY922hOQnpXcam9OgiF0',
+    'admin_id' => '5691972852',
+    'database' => [
+        'host' => 'localhost',
+        'name' => 'hootibot',
+        'user' => 'root',
+        'pass' => '',
+    ],
+];
